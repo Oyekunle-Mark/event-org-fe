5a059e6283f977a2a00157462a763163cc35bd1c
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-02-05 19:18:59 +0100

    Deletes an event

--- src/services/events.ts
+++ src/services/events.ts
@@ -12,20 +12,26 @@ export interface Event {
 const URL = "https://events-man.herokuapp.com/";
 
 export class EventService {
-    public async getEvents(): Promise<AxiosResponse<any>> {
+    public async getEvents(): Promise<AxiosResponse<Event[]>> {
         const response = await axios.get(URL);
 
         return response;
     }
 
-    public async createEvents(event: Event): Promise<AxiosResponse<any>> {
+    public async createEvents(event: Event): Promise<AxiosResponse<Event>> {
         const response = await axios.post(URL, event)
 
         return response;
     }
 
-    public async updateEvents(event: Event, id: number): Promise<AxiosResponse<any>> {
+    public async updateEvents(event: Event, id: number): Promise<AxiosResponse<Event>> {
         const response = await axios.put(`URL/${id}`, event)
+
+        return response;
+    }
+
+    public async removeEvents(id: number): Promise<AxiosResponse<Event>> {
+        const response = await axios.delete(`URL/${id}`)
 
         return response;
     }
